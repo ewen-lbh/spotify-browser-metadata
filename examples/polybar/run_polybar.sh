65de5fc573a07dd3f96dc972a879fcfe5e002d30
Ewen Le Bihan <hey@ewen.works>
2021-04-23 00:23:17 +0200

    :memo: Remove usage of `ii` in examples/polybar

--- examples/polybar/run_polybar.sh
+++ examples/polybar/run_polybar.sh
@@ -9,7 +9,7 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch spotify metadata receiver script
 # TODO: kill previous instances within spotify-metadata-receiver.py itself
 kill $(pgrep -af "python $HOME/.config/polybar/spotify-metadata-receiver.py" | cut -d' ' -f1)
-python ~/.config/polybar/spotify-metadata-receiver.py localhost 8887 '{i("♥ ", liked)}{ii("→ ", repeat)}{i("¹↷", loop)}{i("⇄", shuffle)}{artist} — {title}'
+python ~/.config/polybar/spotify-metadata-receiver.py localhost 8887 '{i("♥ ", liked)}{i("→ ", not repeat)}{i("¹↷", loop)}{i("⇄", shuffle)}{artist} — {title}'
 
 # Launch Polybar
 polybar -c ~/.config/polybar/config.ini top &
