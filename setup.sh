5eda3f4383c357bc6170c12428089be9c636c63f
Ewen Le Bihan <hey@ewen.works>
2021-04-23 12:50:52 +0200

    🚧 Continue working on setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # for qute only for now
-cp scraper.js ~/.config/qutebrowser/greasemonkey/spotify-metadata-scraper.js
-cp receiver.py ~/.config/polybar/spotify-metadata-receiver.py
+if [[ $(xdg-mime query default x-scheme-handler/http) = org.qutebrowser.qutebrowser.desktop ]]; then
+	cp scraper.js ~/.config/qutebrowser/greasemonkey/spotify-metadata-scraper.js
+	cp receiver.py ~/.config/polybar/spotify-metadata-receiver.py
+else if # WIP #
